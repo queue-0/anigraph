f9201510effd1299d734c312ff2e18c6ba1b1c7a
queue-0 <quantumbullet39@gmail.com>
2026-04-01 22:02:37 -0400

    Update graph.js

--- js/graph.js
+++ js/graph.js
@@ -23,6 +23,8 @@ function initGraph() {
   const wrap = document.getElementById('graph-canvas');
 
   graphInstance = ForceGraph()(wrap)
+	.graphData({ nodes: [], links: [] }) // Initialize with empty data
+	.d3Force('center', d3.forceCenter()) // Force nodes toward the middle
     .backgroundColor('#0d0a07')
     .nodeRelSize(6)
     .nodeColor(n => resolveNodeColor(n))
